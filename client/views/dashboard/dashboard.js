add7a26138f72966fca21c05d13f1e10fbcd04b2
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-16 16:36:17 +0200

    Insert into db.

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -1,3 +1,13 @@
 Template.dashboard.rendered = function() {
 
 };
+Template.dashboard.events({
+    'keyup input[type=text]': function (event, template) {
+        if (event.which === 27 || event.which === 13) {
+            event.preventDefault();
+            var project = {};
+            project.name = template.find("#projectNameEnter").value;
+            Meteor.call('saveProject', project);
+        }
+    }
+});
