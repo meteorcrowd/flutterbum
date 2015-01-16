b1d542072d7483d1e9c505518282a0a756943700
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-16 14:55:31 +0200

    indentation

--- client/helpers/stringHelpers.js
+++ client/helpers/stringHelpers.js
@@ -1,8 +1,8 @@
 Template.registerHelper('siteTitle', function(string) {
-  return SEO.settings.title;
+    return SEO.settings.title;
 });
 
 Template.registerHelper('summarize', function(string) {
-  var cleanString = _(string).stripTags();
-  return _(cleanString).truncate(140);
+    var cleanString = _(string).stripTags();
+    return _(cleanString).truncate(140);
 });
