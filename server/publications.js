465961af52c938c682ebb9bbd07e9e83e5150dda
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-17 19:13:16 +0200

    Project related publications

--- server/publications.js
+++ server/publications.js
@@ -16,3 +16,18 @@ Meteor.publishComposite("items", function() {
 Meteor.publish('projects', function () {
     return Projects.find({});
 });
+Meteor.publish('customers', function () {
+    return Customers.find();
+});
+Meteor.publish('calevents', function (project) {
+    return Calevents.find({project: project});
+});
+Meteor.publish('conversations', function (project) {
+    return Conversations.find({project: project});
+});
+Meteor.publish('todos', function (project) {
+    return Todos.find({project: project});
+});
+Meteor.publish('uploads', function (project) {
+    return Uploads.find({project: project});
+});
